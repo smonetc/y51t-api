e617bd605ba372d7870b16f390f97e02d8a1be49
Monet Cooper <smomoc@monets-mbp-2.lan>
2020-10-23 01:14:32 -0400

    migrations set and inserted

---
+++ seeds/seed.category.sql
@@ -0,0 +1,4 @@
+INSERT INTO category (category)
+VALUES
+('Aliens'),
+('UFO');
---
+++ seeds/seed.sightings.sql
@@ -0,0 +1,19 @@
+INSERT INTO sightings(location_name,date_viewed,category_id,content,username)
+VALUES
+('Tennessee','10/17/2020',2,'Sighting of exactly 12 UFOs. The location was Jackson, TN','Anonymous'),
+('Nevada','10/15/2020',2,'UFO hovers over Area 51 in Hiko, NV','Brandon'),
+('North Carolina','09/15/2020',1,'Alien seen in South Park shopping mall in Charlotte, NC','Bridget'),
+('Nevada','12/21/2018',2,'Around 10pm I was out on the street walking home and the street lights started to flicker. I looked up and saw three dots in the sky flying around in a sychronized motion. Then they shot up into the sky and disappeared and all the street lights around went out. Reno, NV','Garry'),
+('Canada','02/29/2020',2,'Bright object hovering in same area for about 15 minutes in Gatineau, QC,CA','Anonymous'),
+('California','10,10,2020',2,'An object like a drone but quiet and much higher in the sky, more like a satellite in Landers,CA','Shelby'),
+('Colorado','08/17/2020',2,'Triangle UFO flew over my house in Denver, CO','Jack'),
+('London','09/19/2020',2,'Mysterious light over London sky, could it be a UFO? In London, England','Benjy'),
+('Arizona','05/01/2017',1,'Tall grey alien sighting when visiting Arizona deserts! Most bizarre sighting! Sonoran Desert, Arizona','Julio'),
+('Kansas','07/29/2018',1,'I was abducted by aliens one night around 1am. I heard a loud noise near my shed and went to investigate. I was taken away for over 72 hours and my wife had the cops looking for me.','Bo'),
+('Texas','07/08/2019',1,'I am a sterile man who went away for three months and came back home to my wife being pregnant. MUST be aliens, I think my son is half alien he looks nothing like me! Fort Worth, TX','Anonymous'),
+('Utah','01/01/2018',2,'Witnessed a Alien craft going through a portal at 6:30am New Years Day! I never believed in aliens until that day! They are out there. Salt Lake City, UT','Rose'),
+('Colorado','10/15/2019',1,'Me and other multiple people saw three glowing orbs hovering over Breckenridge, CO.','Anonymous'),
+('Georgia','10/12/2019',1,'Went to a concert one evening and the indie artist looked a bit weird! Like an alien, is it possible?','Micah'),
+('Montana','10/10/2020',2,'Saw my first UFO sighting up close and personal. Hovered in the sky in front of my car as I was driving.','Christopher'),
+('Montanta','04/04/2019',1,'THERE WAS A CROP CIRCLE IN MY FARM LAST YEAR!','Anonymous'),
+('Canada','10/19/2020',2,'I witnessed a dark large triangle craft in Penticton,BC,CA!','Anonymous');
