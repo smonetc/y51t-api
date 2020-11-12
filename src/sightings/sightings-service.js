c6b754de4e5c7245df66d92998495035c25777dc
Monet Cooper <smomoc@monets-mbp-2.lan>
2020-11-11 23:18:44 -0500

    updated sightings-service.js

--- src/sightings/sightings-service.js
+++ src/sightings/sightings-service.js
@@ -2,7 +2,7 @@ const SightingsService = {
     getAllSightings(knex){
         return knex
         .raw(`SELECT s.id, s.location_name,s.date_viewed, s.content, s.username, ca.category FROM sightings s
-        LEFT JOIN category ca ON s.category_id = ca.id`)
+        LEFT JOIN category ca ON s.category_id = ca.id ORDER BY s.id`)
     },
     insertSighting(knex, newSightings) {
         return knex
